9403fe27a5373182be607be6deff5aa434d8fd2a
Evan <evansmalley2@gmail.com>
2024-02-06 18:14:57 -0500

    Bug fix: serverAPI caching issue

--- components/serverAPI.ts
+++ components/serverAPI.ts
@@ -12,14 +12,14 @@ type Config = {
 
 let config: Config | null = null;
 try {
-	config = require('../serverConfig');
+  config = require('../serverConfig');
 } catch (e) {
-	config = {
-		'host': 'localhost',
-	  'port': 5000,
-	  'http': 'http',
-		'secret': null,
-	};
+  config = {
+    'host': 'localhost',
+    'port': 5000,
+    'http': 'http',
+    'secret': null,
+  };
 }
 const protocol = (config && config.http) || 'http';
 const hostname = (config && config.host) || 'localhost';
@@ -27,32 +27,29 @@ const port = (config && config.port) || 5000;
 const secret = (config && config.secret) || null;
 
 type OptionalFetchArgs = {
-	revalidate: number;
-	tags?: string[];
+  revalidate: number;
+  tags?: string[];
 };
 
 export async function useServerAPI(args, optional_fetch_args = {} as OptionalFetchArgs) {
-	const request = JSON.stringify(args);
+  const request = JSON.stringify(args);
 
-	const cachedLocation = 'API.REQUESTS.' + request;
+  const cachedLocation = 'API.REQUESTS.' + request;
 
-	let isCached = false;
-	let cacheSeconds = 0;
-	let cache = {};
+  let isCached = false;
+  let cacheSeconds = 0;
+  let cache = null;
 
-	if (optional_fetch_args && optional_fetch_args.revalidate) {
-		cacheSeconds = optional_fetch_args.revalidate;
-		cache = cacheData.get(cachedLocation);
-	}
+  if (optional_fetch_args && 'revalidate' in optional_fetch_args) {
+    cacheSeconds = optional_fetch_args.revalidate;
+    cache = cacheData.get(cachedLocation);
+  }
 
-	if (cache !== null) {
-		isCached = true;
-	}
+  if (cache !== null) {
+    isCached = true;
+  }
 
-	// console.log(cachedLocation)
-	// console.log(isCached)
-
-	const headers: HeadersInit = {
+  const headers: HeadersInit = {
     'Content-Type': 'application/json',
   };
 
@@ -60,39 +57,38 @@ export async function useServerAPI(args, optional_fetch_args = {} as OptionalFet
     headers['X-SECRET-ID'] = secret;
   }
 
-
   let data: object = {};
 
-	if (isCached === false) {
-		// console.log('MISS')
-		data = await fetch(protocol + '://' + hostname + ':' + port, Object.assign(
-			{
-				next: {revalidate: 0}
-			},
-			optional_fetch_args,
-			{
-				'method': 'POST',
-				'headers': headers,
-				'body': request,
-			}
-		)).then(response => {
-			return response.json();
-		}).then(json => {
-			return json;
-		}).catch(error => {
-			console.log(error);
-			throw new Error('Error');
-		});
+  if (isCached === false) {
+    // console.log('MISS')
+    data = await fetch(protocol + '://' + hostname + ':' + port, Object.assign(
+      {
+        next: {revalidate: 0}
+      },
+      optional_fetch_args,
+      {
+        'method': 'POST',
+        'headers': headers,
+        'body': request,
+      }
+    )).then(response => {
+      return response.json();
+    }).then(json => {
+      return json;
+    }).catch(error => {
+      console.log(error);
+      throw new Error('Error');
+    });
 
-		if (cacheSeconds > 0) {
-			cacheData.put(cachedLocation, data, 1000 * cacheSeconds);
-		}
-	} else {
-		// console.log('HIT')
-		data = cache;
-	}
+    if (cacheSeconds > 0) {
+      cacheData.put(cachedLocation, data, 1000 * cacheSeconds);
+    }
+  } else {
+    // console.log('HIT')
+    data = cache || {};
+  }
 
-	return data;
+  return data;
 };
 
 
