b2761de248376bd3e30436fa875da86bfaa58b85
Evan <evansmalley2@gmail.com>
2024-08-08 22:06:56 -0400

    cpu threads

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,9 +1,11 @@
+// todo maybe remove this? not sure if it is any useful
+// if I want to use it, should check the ENV variables to make sure stripe pulic key etc still work, since it is setting an env variable
 module.exports = {
   apps: [
     {
       name: 'next',
       script: 'npm',
-      args: 'start',
+      args: 'start -p 3000',
       instances: 'max',
       exec_mode: 'cluster',
       env: {
