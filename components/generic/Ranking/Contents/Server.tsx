61ced3aadc5428446b42a55042f5b44da2afb48b
Evan <evansmalley2@gmail.com>
2025-06-12 20:02:24 -0400

    caching

--- components/generic/Ranking/Contents/Server.tsx
+++ components/generic/Ranking/Contents/Server.tsx
@@ -5,7 +5,7 @@ import { useServerAPI } from '@/components/serverAPI';
 import DataHandler from '../DataHandler';
 
 const Server = async ({ organization_id, division_id, season, view }) => {
-  const seconds = 60 * 60 * 5; // cache for 5 hours
+  const seconds = 60 * 60; // cache for 1 hours
 
   let fxn = 'getTeamRanking';
   if (view === 'player') {
@@ -36,13 +36,10 @@ const Server = async ({ organization_id, division_id, season, view }) => {
       function: dataArgs.function,
       arguments: dataArgs.arguments,
     },
-    // cache: seconds,
+    cache: seconds,
   };
 
 
-  // todo once this goes live, test how long it takes... the query *should* be fast,
-  // but it is hard to tell the transport time to get the data from mysql -> node -> client,
-  // if mysql -> node is too slow then bring back node cache
   const data = await useServerAPI(cacheArgs);
 
   const generated = new Date().getTime();
