a9a0131b74f840614ed5c0b526f2369b2f5961de
Evan <evansmalley2@gmail.com>
2025-10-09 19:31:10 -0400

    twitter

--- app/layout.tsx
+++ app/layout.tsx
@@ -65,6 +65,14 @@ export default async function RootLayout({ children }: {children: React.ReactNod
               gtag('config', 'G-S67JFT2KZW');
             `}
           </Script>
+          <Script id="twitter-conversion" strategy="afterInteractive">
+            {`
+              !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+},s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+twq('config','qltj2');
+            `}
+          </Script>
         </LayoutWrapper>
       </StoreProvider>
     </html>
